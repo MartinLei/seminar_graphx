5dd9f91bab84c44ed911445f2cc043683657a331
Martin lei <17988093+MartinLei@users.noreply.github.com>
2023-04-15 16:54:01 +0200

    init

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,18 @@
+package de.seminar.graphx
+
+object Main {
+
+  private final val PEOPLE_CSV_PATH = "W:\\HTWG_Master\\Seminar\\data\\people.csv"
+  private final val PEOPLE_JSON_PATH = "W:\\HTWG_Master\\Seminar\\data\\people.json"
+  def main(args: Array[String]): Unit = {
+
+//    val spark = SparkSession.builder
+//      .appName("Spark SQL Seminar")
+//      .config("spark.master", "local")
+//      .getOrCreate()
+
+
+
+  }
+
+}
